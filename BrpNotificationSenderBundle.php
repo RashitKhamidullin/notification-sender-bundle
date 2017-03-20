9f46f80f11658dc031c02c204b7f42f29c8d0e47
Rashit Hamidullin <crush.in.side@gmail.com>
2017-03-20 10:43:23 +0300

    Initial commit

---
+++ BrpNotificationSenderBundle.php
@@ -0,0 +1,10 @@
+<?php
+namespace Brp\NotificationSenderBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BrpNotificationSenderBundle extends Bundle
+{
+
+}
